cb655a128549cbbd1305f5bec318901281e8227e
Junaid Qureshi <Junaid.qureshi1995@gmail.com>
2016-10-04 15:31:27 -0500

    First Commit

---
+++ Assets/Code/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+    GameObject player;
+	// Use this for initialization
+	void Start () {
+        player = GameObject.Find("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(player.transform.position.x > -15 && player.transform.position.x < 15)
+        {
+            if(Character.direction == "Left")
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * Character.speed);
+            }
+            if(Character.direction == "Right")
+            {
+                transform.Translate(Vector3.right * Time.deltaTime * Character.speed);
+            }
+        }
+        if(Character.direction == "Down")
+        {
+            transform.Translate(Vector3.down * Time.deltaTime * Character.speed);
+        }
+	}
+}
---
+++ Assets/Code/UI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Diagnostics;
+
+public class UI : MonoBehaviour {
+    public Text Display;
+    public Stopwatch Timer;
+    public static double time;
+	// Use this for initialization
+	void Start () {
+        Display = GetComponent<Text>();
+        Timer = new Stopwatch();
+        Timer.Start();
+        time = 0;
+       
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time = (int)Timer.Elapsed.TotalSeconds;
+        Display.text = "Timer: " + time + "\tHealth: ";
+	}
+}
