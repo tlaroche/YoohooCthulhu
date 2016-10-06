474f73057fad194afcdd54987fdfba2a44eee010
Junaid-Qureshi <Junaid.qureshi1995@gmail.com>
2016-10-06 15:48:05 -0500

    Add files via upload

---
+++ CameraTrack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrack : MonoBehaviour {
+    public Transform cameraTrack;
+    public static int yvalue;
+    // Use this for initialization
+    void Start()
+    {
+        cameraTrack = GameObject.Find("Camera Track").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = cameraTrack.position;
+        pos.z = transform.position.z;
+        transform.position = pos;
+        //transform.position = new Vector3(Mathf.Clamp(transform.position.x, -15, 15), (transform.position.y), transform.position.z);
+    }
+}
---
+++ Rock.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rock : MonoBehaviour {
+
+    public GameObject Player;
+    private Character Playerhealth;
+    // Use this for initialization
+    void Start () {
+        Player = GameObject.FindGameObjectWithTag("Player");
+        Playerhealth = Player.GetComponent<Character>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (Character.direction == "Down")
+        {
+            Character.direction = "Left";
+        }
+        else if (Character.direction == "Left" || Character.direction == "Right")
+        {
+            Character.direction = "Down";
+        }
+    }
+}
